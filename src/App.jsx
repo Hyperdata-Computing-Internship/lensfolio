8c126e31b8c40ff06759f1e4bb39920a1f61b4a8
Syed Zaigham Hussain Shah <zaigham.shah2004@gmail.com>
2026-09-15 16:28:01 +0500

    working on pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
   const [data, setdata] = useState([])
+  const [page, setpage] = useState(1)
 
   const getdata = async () => {
-    const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=100")
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${page}&limit=100`)
     setdata(response.data)
   }
-  let printuserdata = "no data to display"
+  let printuserdata = "Laoding"
   if (data.length > 0) {
     printuserdata = data.map(function (elem, idx) {
       return <div key={idx}>
@@ -22,6 +23,10 @@ const App = () => {
     })
   }
 
+  useEffect(() => {
+    getdata();
+  }, [page])
+
 
 
 
@@ -29,18 +34,51 @@ const App = () => {
 
   return (
     <div className='bg-black overflow-auto h-screen p-4 text-white'>
-      <button
+      {/* <button
         onClick={() => {
           getdata()
         }}
         className='bg-green-600 active:scale-95 mb-3 px-5 py-2 rounded text-white'
       >
         Get data
-      </button>
+      </button> */}
+
+      {/* data displlay */}
 
       <div className='flex flex-wrap gap-4'>
         {printuserdata}
       </div>
+
+      {/* pagination buttons */}
+
+      <div className='flex justify-center gap-6 items-center p-4'>
+        <button
+          style={{ opacity: page == 1 ? 0.6 : 1 }}
+          className='bg-amber-400 text-sm cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold'
+          onClick={() => {
+            if(page>1){
+              setpage(page-1)
+              setdata([])
+            }
+          }}
+        >
+          Prev
+        </button>
+        <h4>Page {page}</h4>
+        <button
+          className='bg-amber-400 text-sm cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold'
+          onClick={() => {
+            setpage(page+1)
+            setdata([])
+          }}
+        >
+          Next
+        </button>
+      </div>
+
+
+
+
     </div>
   )
 }
